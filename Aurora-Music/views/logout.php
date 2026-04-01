2cdc9045dd046bcfded1912ba1232b532382ae4a
Clayton Campos <30420230+x-gyba@users.noreply.github.com>
2026-04-01 15:34:15 -0300

    Delete Aurora-Music directory

--- Aurora-Music/views/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
-header("Location: /Aurora-Music/");
-exit;
